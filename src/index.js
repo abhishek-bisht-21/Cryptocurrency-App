b85855dedc79d53da3c34d6ec2c560f936d9c18d
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-29 00:00:25 +0530

    Setting up redux-toolkit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.render(<Router><App/></Router>,document.getElementById('root'));
+import 'antd/dist/antd.css';
+import store from './app/store';
+
+ReactDOM.render(
+<Router>
+	<Provider store={store}>
+	<App/>
+	</Provider>
+</Router>,
+document.getElementById('root'));
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,18 @@
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     tiers: '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+//     'X-RapidAPI-Key': '5b17d0285cmsh82bf09aaaefd7ffp19dadcjsndfb54f700bf7'
+//   }
+// };
+
