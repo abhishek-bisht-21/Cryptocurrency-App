670b4f5ac47ae816287b8ebec91f78b745352f58
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-28 21:51:37 +0530

    Basic structure of app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Switch,Route,Link } from 'react-router-dom';
+import { Typography,Layout,Space } from 'antd';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+	<div className="navbar"></div>
+	<div className="main"></div>   
+  	<div className="footer"></div>   
+    </div>
   )
 }
 
